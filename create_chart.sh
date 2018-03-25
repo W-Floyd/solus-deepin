f1261fb8dc80ebf0311bdc99e30b30134c8993d3
William Floyd <william.png2000@gmail.com>
2018-03-25 00:28:59 -0400

    create_chart: Show both run/build deps at the same time.

--- create_chart.sh
+++ create_chart.sh
@@ -10,6 +10,25 @@
 # libqcef libqcef
 # Qt5Xdg libqtxdg
 
+__tmpfile="$(mktemp)"
+__tmpfile2="$(mktemp)"
+
+while read -r __type; do
+    while read -r __line; do
+        __parent="${__line/ *}"
+        __child="${__line/* }"
+        echo "    \"${__parent}\" -> \"${__child}\"[${__type}];" | sed 's/-devel"/"/g'
+    done < "${__type}_deps"
+done <<< 'run
+build' | sort > "${__tmpfile}"
+
+cp "${__tmpfile}" "${__tmpfile2}"
+
+sed 's#\[.*##' < "${__tmpfile}" | uniq -d | while read -r __line; do
+    sed -e "s#^${__line}\[.*##" -i "${__tmpfile2}"
+    echo "${__line}[run_build];" >> "${__tmpfile2}"
+done
+
 {
 
 echo 'digraph {
@@ -20,22 +39,18 @@ echo 'digraph {
     node [style=filled, shape=record, color="black" fillcolor="none" ]
 '
 
-while read -r __type; do
-    while read -r __line; do
-        __parent="${__line/ *}"
-        __child="${__line/* }"
-        echo "    \"${__parent}\" -> \"${__child}\"[${__type}];"
-    done < "${__type}_deps"
-done <<< 'run
-build'
+sort "${__tmpfile2}"
 
 echo '}'
 
 } > graph.dot
 
+rm "${__tmpfile}" "${__tmpfile2}"
+
 sed -e 's/\[run\]/[color=blue]/' \
 -e 's/\[done\]/\[fillcolor=darkolivegreen1\]/' \
 -e 's/\[build\]/\[color=red\]/' \
+-e 's/\[run_build\]/\[color="red:blue"\]/' \
 -e 's/\[modified\]/\[fillcolor=orange\]/' \
 -e 's/\[new\]/\[fillcolor=limegreen\]/' \
 -e 's/\[error\]/\[fillcolor=tomato\]/' graph.dot | neato -Ln5 -Tgtk
